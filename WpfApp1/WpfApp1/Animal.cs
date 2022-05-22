5693046b02cfd230eb9cbcd0c78e52bb875f5ca3
Dodo birds <81391324+Danil-python-it@users.noreply.github.com>
2022-05-22 16:23:15 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/Animal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
+namespace шещп
+{
+    class Animal
+    {
+        public int width = 0, height = 0;
+        public SolidColorBrush color = new SolidColorBrush(Colors.Red);
+        public Rectangle body = new Rectangle();
+        public int left = 0, top = 0,speed = 0;
+        public int MoveLeft = 0,MoveTop = 0;
+        public Animal(int w, int h, int l, int t, int s = 1)
+        {
+            width = w;
+            height = h;
+            left = l;
+            top = t;
+            speed = s;
+        }
+        public void SetColor(SolidColorBrush c)
+        {
+            this.color = c;
+        }
+        public void Translaiet(int a, int b)
+        {
+            this.left += a;
+            this.top += b;
+            this.body.Margin = new System.Windows.Thickness(this.left, this.top, 0, 0);
+        }
+        public void Load()
+        {
+            this.body.Margin = new System.Windows.Thickness(this.left, this.top, 0, 0);
+            this.body.Width = this.width;
+            this.body.Height = this.height;
+            this.body.Fill = this.color;
+        }
+        public void Update()
+        {
+            if (this.MoveLeft != this.left || this.MoveTop != this.top)
+            {
+                if (this.left < this.MoveLeft) { this.left += this.speed; }
+                if (this.left > this.MoveLeft) { this.left -= this.speed; }
+                if (this.top < this.MoveTop) { this.top += this.speed; }
+                if (this.top > this.MoveTop) { this.top -= this.speed; }
+                this.body.Margin = new System.Windows.Thickness(this.left, this.top, 0, 0);
+            }
+            if (this.MoveLeft == this.left && this.MoveTop == this.top)
+            {
+                Random rand = new Random();
+                this.MoveLeft = this.left + rand.Next(-50, 50);
+                this.MoveTop = this.top + rand.Next(-50, 50);
+            }
+        }
+        public Rectangle Get()
+        {
+            return this.body;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace шещп
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, Animal>Animals = new Dictionary<string, Animal>(0);
+        string MouseStatus = "CreateNewAnimal";
+        int index = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow1_Activated(object sender, EventArgs e)
+        {
+            foreach(var i in Animals)
+            {
+                if (i.Value.MoveLeft != i.Value.left || i.Value.MoveTop != i.Value.top)
+                {
+                    if (i.Value.left < i.Value.MoveLeft) { i.Value.Translaiet(i.Value.speed,0);}
+                    if (i.Value.left > i.Value.MoveLeft) { i.Value.Translaiet(i.Value.speed*-1, 0); }
+                    if (i.Value.top < i.Value.MoveTop) { i.Value.Translaiet(0,i.Value.speed); }
+                    if (i.Value.top > i.Value.MoveTop) { i.Value.Translaiet(0,-1*i.Value.speed); }
+                    
+                }
+                if (i.Value.MoveLeft == i.Value.left && i.Value.MoveTop == i.Value.top)
+                {
+                    Random rand = new Random();
+                    i.Value.MoveLeft = i.Value.left + rand.Next(-50, 50);
+                    i.Value.MoveTop = i.Value.top + rand.Next(-50, 50);
+                }
+            }
+        }
+
+        private void MainWindow1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (MouseStatus == "CreateNewAnimal")
+            {
+                Random rand = new Random();
+                SolidColorBrush color = new SolidColorBrush(Color.FromRgb(Convert.ToByte(rand.Next(1, 255)), Convert.ToByte(rand.Next(1, 255)), Convert.ToByte(rand.Next(1, 255))));
+                Animal temp = new Animal(4,2, Convert.ToInt32(e.GetPosition(can).X), Convert.ToInt32(e.GetPosition(can).Y));
+                temp.SetColor(color);
+                temp.Load();
+                can.Children.Add(temp.Get());
+                Animals.Add(index.ToString(),temp);
+                index++;
+            }
+
+        }
+
+        private void NewAnim_Click(object sender, RoutedEventArgs e)
+        {
+            MouseStatus = "CreateNewAnimal";
+        }
+
+        private void NewTree_Click(object sender, RoutedEventArgs e)
+        {
+            MouseStatus = "CreateNewTrees";
+        }
+
+        private void None_Click(object sender, RoutedEventArgs e)
+        {
+            MouseStatus = "none";
+        }
+    }
+}
